1762218948b13b1aa87bd61904a888e4a6ed41ba
learnwithjuni <support@learnwithjuni.com>
2025-01-14 08:11:19 -0800

    Add files via upload

---
+++ JS-Check-in-1-Additional-Project-Starter/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    // Get the coordinate from the user
+    Scanner in = new Scanner(System.in);
+    System.out.print("Please input a number from 0 to 10: ");
+    int row = in.nextInt();
+    System.out.print("Enter another number from 0 to 10: ");
+    int col = in.nextInt();
+
+    // Put an "O" if in the same row and column, but put an "X" at the entered coordinate
+    
+  }
+}
